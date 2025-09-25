9558b179d8322381be3df94ea78032ca72dab97e
kstrong03 <kat.strong2003@gmail.com>
2025-09-25 10:59:37 -0400

    added MovingWall Script

---
+++ Assets/_Scripts/MovingWall.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class MovingWall : MonoBehaviour
+{
+
+    public float moveSpeed = 2f; //How fast it moves
+    public float moveRange = 5f; //The horizontal range of where it can move
+
+    private Vector3 startPosition;
+    private bool movingRight = true;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
